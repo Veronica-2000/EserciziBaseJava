fd29ad41131ce11dba8cf16ca3997b83439f673b
Veronica-2000 <veronicagioia@icloud.com>
2025-04-23 19:46:03 +0200

    caricato esercizio Base2

---
+++ EsercizioBase2.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class EsercizioBase2 {
+
+	public static void main(String[] args) {
+		
+		Random r=new Random();
+		int n1=r.nextInt(101);
+		int n2=r.nextInt(101);
+		
+		int somma=n1+n2;
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(somma);
+
+	}
+
+}
+
+
+/*
+Effettuare un operazione matematica tra 
+due numeri interi e stampare il risultat
+*/
