0cc569916fc6d7cf4f3e7e791fe9659d12cf4b1a
Veronica-2000 <veronicagioia@icloud.com>
2025-04-25 10:38:01 +0200

    EsercizioBase4

---
+++ EsercizioBase4.java
@@ -0,0 +1,16 @@
+
+public class EsercizioBase4 {
+
+	public static void main(String[] args) {
+		
+		String par="Ciao";
+		boolean b=true;
+		System.out.println(par + b);
+		
+
+	}
+
+}
+
+
+//definire una variabile di tipo boleana e stampa il valore  preceduto da una stringa
